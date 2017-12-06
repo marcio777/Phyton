938fae0aaa5d604d1ccd4c1fb88309977c9b6514
marcio777 <marcio.infra@hotmail.com>
2017-12-05 22:59:03 -0200

    inicial

---
+++ Aula1.py
@@ -0,0 +1,10 @@
+n1 = int(input('Digite um valor : '))
+n2 = int(input('Digite outro valor: '))
+som = n1+n2
+sub = n1-n2
+mul = n1*n2
+div = n1/n2
+
+print('A soma é {},\n A multiplicação é {},\n A subtração é {},\n A divisão é {}'.format(som,mul,sub,div))
+
+
